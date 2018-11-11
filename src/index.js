a076ab8dff126853ba4f7fa0544c6974d77ca18d
jerryHZLiang <798895110@qq.com>
2018-11-11 15:39:12 +0800

    删除 Welcome

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Welcome from './Welcome';
+import App from './App';
 
 ReactDOM.render(
-    <Welcome name="hzl"/>,
+    <App/>,
   document.getElementById('root')
 )
 // If you want your app to work offline and load faster, you can change
