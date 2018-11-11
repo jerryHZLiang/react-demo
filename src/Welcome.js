173eea426482170c8e11c0983e4b8087ba255511
jerryHZLiang <798895110@qq.com>
2018-11-11 14:42:46 +0800

    尝试改变 state

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,24 @@
 import React from 'react'; // 为什么要 import React
 class Welcome extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+          date: new Date()
+        }
+        setInterval(()=>{ // 搜索「JS 箭头函数 MDN」
+            this.state = {
+              date: new Date() // 更新 date
+            }
+          })
+      }
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+    return (
+        <div>
+          <h1>Hello, {this.props.name}</h1>
+          <h2>{this.state.date.toString()}</h2>
+        </div>
+      )  }
 }
 
 export default Welcome // 为什么要 export，为什么要加 default
