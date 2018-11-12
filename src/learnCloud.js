57c0cc0bf2d6cffc9f9f1ddc5cbf289674e6d87c
jerryHZLiang <798895110@qq.com>
2018-11-12 23:42:00 +0800

    新建 leanCloud.js

---
+++ src/learnCloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage';
+var APP_ID = '8obWURMfq0Ib9UAsArl4N5bq-gzGzoHsz';
+var APP_KEY = 'sWejjAhhN5048WSdX7BF4Idd';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+export default AV;
