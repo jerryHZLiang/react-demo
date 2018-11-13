5aba0d8c0e743ef102afd7a5bf2352f2fcb376fc
jerryHZLiang <798895110@qq.com>
2018-11-13 14:59:43 +0800

    添加登录/注册对话框-UserDialog.js ,css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'normalize.css';
 import './reset.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
-
+import UserDialog from './UserDialog'
 
 class App extends Component {
   constructor(props){
@@ -38,6 +38,7 @@ class App extends Component {
       <ol className="todoList">
         {todos}
       </ol>
+      <UserDialog />
       </div>
     )
   }
